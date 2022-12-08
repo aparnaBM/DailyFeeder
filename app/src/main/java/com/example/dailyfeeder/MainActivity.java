dd10530a58db550ba0b8c306977215d6de8e24f8
madhav_aipl <madhav.ghatge@anant.co.in>
2022-12-08 10:29:55 +0530

    DailyFeeder app design changes

--- app/src/main/java/com/example/dailyfeeder/MainActivity.java
+++ app/src/main/java/com/example/dailyfeeder/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.dailyfeeder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnSub;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnSub = findViewById(R.id.btnSubmit);
+        btnSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, OutputActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
